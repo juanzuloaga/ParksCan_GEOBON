a6dc58bdc080e038a2354230d86b7563881c294a
juanzuloaga <jzuloaga65@gmail.com>
2023-05-17 22:03:33 -0600

    new html docs

--- mapsR.R
+++ mapsR.R
@@ -3,7 +3,7 @@ library(sf)
 library(magrittr)
 library(geojsonR)
 library(geojsonsf)
-library(wdpar)
+#library(wdpar)
 
 
 #Canada <-  geojsonsf::geojson_sf("https://geoportal.gov.mb.ca/datasets/manitoba::canada-provinical-boundaries.geojson?outSR=%7B%22latestWkid%22%3A3857%2C%22wkid%22%3A102100%7D")%>%
